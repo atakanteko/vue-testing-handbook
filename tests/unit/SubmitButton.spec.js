40dffd2931597842ac7e011836b05b1c82ec8f6c
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-14 00:54:27 +0300

    refactor settings props with propsData

--- tests/unit/SubmitButton.spec.js
+++ tests/unit/SubmitButton.spec.js
@@ -2,18 +2,28 @@ import { mount } from "@vue/test-utils";
 import SubmitButton from "../../src/components/SubmitButton";
 
 
+const msg = "submit"
+const factory = (propsData) => {
+    return mount(SubmitButton,{
+        propsData:{
+            ...propsData
+        }
+    })
+}
 
 describe('SubmitButton.vue', () => {
+
     it('should display a non authorized message', function () {
-        const msg = 'submit';
-        const wrapper = mount(SubmitButton, {
-            propsData:{
-                msg: msg,
-                isAdmin: true
-            }
-        })
-        console.log(wrapper.html())
-        expect(wrapper.find("span").text()).toBe("Admin Privileges")
+        const msg = 'submit'
+        const wrapper = factory({msg: msg, isAdmin: false})
+        expect(wrapper.find("span").text()).toBe("Not Authorized")
         expect(wrapper.find("button").text()).toBe("submit")
     });
+
+    it("renders a message", () => {
+        const wrapper = factory({msg: msg, isAdmin: true })
+
+        expect(wrapper.find("span").text()).toBe("Admin Privileges")
+        expect(wrapper.find("button").text()).toBe("submit")
+    })
 })
