c033bf2d5cb2b5015bdb36dda099b858dc706699
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-15 00:07:07 +0300

    jest.mock()

---
+++ tests/unit/AxiosCall.spec.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { BASE_URL, fetchUsers } from "../../utils/service"
+
+jest.mock("axios");
+
+describe("fetchUsers", () => {
+    describe("API call is successful", () => {
+        it('should return user list', async function () {
+            // given
+            const users = [
+                { id: 1, name: "John" },
+                { id: 2, name: "Andrew" },
+            ];
+            axios.get.mockResolvedValueOnce(users);
+
+            // when
+            const result = await fetchUsers();
+            console.log(result)
+            // then
+            expect(axios.get).toHaveBeenCalledWith(`${BASE_URL}/users`);
+            expect(result).toEqual(users);
+        });
+    })
+})
