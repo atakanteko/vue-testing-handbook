ad8c25f7eb43474545350153fdcfb215d0ea2130
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-14 22:47:05 +0300

    mocking an ajax call

--- tests/unit/FormSubmitter.spec.js
+++ tests/unit/FormSubmitter.spec.js
@@ -1,14 +1,37 @@
 import { mount } from "@vue/test-utils"
 import FormSubmitter from "@/components/FormSubmitter.vue"
+import flushPromises from "flush-promises"
 
-describe("FormSubmitter.vue",()=>{
-    it('should reveal a notification when submitted', async function () {
-        const wrapper = mount(FormSubmitter)
+let url= ''
+let data= ''
+
+const mockHttp = {
+    get: (_url, _data) => {
+        return new Promise((resolve, reject)=>{
+            url = _url
+            data = _data
+            resolve()
+        })
+    }
+}
+
+
+describe("FormSubmitter.vue", ()=>{
+    it("reveals a notification when submitted", async () => {
+        const wrapper = mount(FormSubmitter, {
+            mocks: {
+                $http: mockHttp
+            }
+        })
 
         await wrapper.find("[data-username]").setValue("alice")
         await wrapper.find("form").trigger("submit.prevent")
 
+        await flushPromises()
+
         expect(wrapper.find(".message").text())
             .toBe("Thank you for your submission, alice.")
-    });
+        expect(url).toBe("/api/v1/register")
+        expect(data).toEqual({ username: "alice" })
+    })
 })
