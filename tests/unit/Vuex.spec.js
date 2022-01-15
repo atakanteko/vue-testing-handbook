ec90e59c0a8c1894db159bcba58240ec22196990
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-15 21:25:14 +0300

    mocking an API and vuex action

--- tests/unit/Vuex.spec.js
+++ tests/unit/Vuex.spec.js
@@ -1,5 +1,22 @@
-import mutation from '../../store/mutations'
 import mutations from "../../store/mutations";
+import actions from "../../store/actions";
+import axios from "axios";
+
+let url=''
+let body={}
+let mockError = false
+jest.mock("axios",()=>({
+    post: (_url,_body) => {
+        return new Promise((resolve, reject) => {
+            if (mockError){
+                throw Error()
+            }
+            url = _url
+            body = _body
+            resolve(true)
+        })
+    }
+}))
 
 describe("SET_POST", () => {
     it('should add a post to the state', function () {
@@ -14,4 +31,28 @@ describe("SET_POST", () => {
             posts: { "1": post }
         })
     });
+
+    it('should authenticated a user', async function () {
+        const commit = jest.fn()
+        const username = "atakan"
+        const password = "pass123"
+
+        await actions.authenticate({commit},{username,password})
+
+        expect(url).toBe("/api/authenticate")
+        expect(body).toEqual({username,password})
+        expect(commit).toHaveBeenCalledWith(
+            "SET_AUTHENTICATED",true)
+
+    });
+    it('should catch an error', async function () {
+        mockError = true
+        await expect(actions.authenticate({commit: jest.fn()}, {}))
+            .rejects.toThrow("API Error occurred.")
+    });
 })
+
+
+
+
+
